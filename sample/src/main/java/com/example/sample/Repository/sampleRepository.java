51bf86d68fa0431d2803818242246ec49073696e
Mohamed Riyaz Ahamed <91612487+riyaz-ahamed-07@users.noreply.github.com>
2026-05-05 09:44:44 +0530

    Delete sample/src/main/java/com/example/sample/Repository/sampleRepository.java

--- sample/src/main/java/com/example/sample/Repository/sampleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.sample.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.sample.Entity.sampleEntity;
-
-public interface sampleRepository extends JpaRepository<sampleEntity, Integer> {
-    
-}
